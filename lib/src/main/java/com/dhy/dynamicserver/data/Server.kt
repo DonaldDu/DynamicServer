7b3c986f693b2e68cff4c3e10ff59e15469747b5
donald <onsemise@qq.com>
2020-08-17 08:53:42 +0800

    fix releaseValue error

--- lib/src/main/java/com/dhy/dynamicserver/data/Server.kt
+++ lib/src/main/java/com/dhy/dynamicserver/data/Server.kt
@@ -1,7 +1,9 @@
 package com.dhy.dynamicserver.data
 
+import com.dhy.dynamicserver.data.RemoteConfig.Companion.releaseServerTypeName
+
 class Server(override val type: String, override val name: String, override val value: String) : IDynamicServer {
     override val releaseValue: String? by lazy {
-        RemoteConfig.dynamicServers.find { it.name == name && it.type == type }?.value
+        RemoteConfig.dynamicServers.find { it.name == name && it.type == releaseServerTypeName }?.value
     }
 }
